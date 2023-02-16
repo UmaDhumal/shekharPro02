1412fb7ad8d29618ac6c596f391e70e8124bec62
admin <admin@DESKTOP-6099UR4>
2023-02-16 20:35:55 +0530

    by Vihaan

---
+++ GIT01/src/com/day01/Demo.java
@@ -0,0 +1,16 @@
+package com.day01;
+
+public class Demo 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Created by Shekhar");
+		
+	}
+	public void m1()
+	{
+		System.out.println("Created by Uma");
+		
+	}
+
+}
